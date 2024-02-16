26501f5dc70d787e49d19728e746857c5baee5b1
Tehaan Perera <134434896+raven8119@users.noreply.github.com>
2024-02-16 13:55:25 +0530

    removed cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SnsModule } from './sns/sns.module';
-import { MiddlewareConsumer } from '@nestjs/common';
-import { CorsMiddleware } from './middleware/cors.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), SnsModule],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(CorsMiddleware)
-      .forRoutes('*');
-  }
-}
+export class AppModule { }
