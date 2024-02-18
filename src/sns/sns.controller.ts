233ec08c76f0e664cbb96e11881470befec356df
Tehaan Perera <134434896+raven8119@users.noreply.github.com>
2024-02-18 20:55:23 +0530

    removed auth guards

--- src/sns/sns.controller.ts
+++ src/sns/sns.controller.ts
@@ -1,10 +1,8 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { SnsService } from './sns.service';
 import { CreateSnDto } from './dto/create-sn.dto';
-import { UrlRestrictionsGuard } from 'src/guards/url-restrictions/url-restrictions.guard';
 
 @Controller('sns')
-@UseGuards(UrlRestrictionsGuard)
 export class SnsController {
   constructor(private readonly snsService: SnsService) {}
 
